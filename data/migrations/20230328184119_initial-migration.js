f792f19eab4651c93aaa0cdb1b5ce9326af6074f
Robert Gripshover <robertgrips@gmail.com>
2023-03-28 15:16:47 -0400

    added half of the data struture to the migrations

--- data/migrations/20230328184119_initial-migration.js
+++ data/migrations/20230328184119_initial-migration.js
@@ -10,6 +10,26 @@ exports.up = async function(knex) {
         table.string('project_description', 300)
         table.boolean('project_completed') //<< this could be wrong check this ???
     })
+    .createTable('resoures', table => {
+        table.increments('resource_id').primary() //<< don't know if I should add this .primary() ???
+        table.string('resource_name').notNullable().unique()
+        table.string('resource_description')
+    })
+    .createTable('tasks', table => {
+        table.increments('task_id').primary() //<< don't know if I should add this .primary() ???
+        table.string('task_description').notNullable()
+        table.string('task_notes')
+        table.boolean('task_completed') //<< this could be wrong check this ???
+        //FK vv
+        table.integer('project_id').notNullable()
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+        //^^ I am pretty sure all of this is the FK pointing to the project_id
+    })
 
 };
 
