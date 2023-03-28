dc91e64df645c2c3bc326ec49eb08f4159489334
Robert Gripshover <robertgrips@gmail.com>
2023-03-28 15:09:22 -0400

    working on the migrations tables

--- data/migrations/20230328184119_initial-migration.js
+++ data/migrations/20230328184119_initial-migration.js
@@ -3,7 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id').primary() //<< don't know if I should add this .primary() ???
+        table.string('project_name', 200).notNullable()
+        table.string('project_description', 300)
+        table.boolean('project_completed') //<< this could be wrong check this ???
+    })
+
 };
 
 /**
