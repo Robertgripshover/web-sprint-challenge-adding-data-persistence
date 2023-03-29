f93dc84664d36c678904f720a36d8bae6490732f
Robert Gripshover <robertgrips@gmail.com>
2023-03-29 10:11:53 -0400

    finished my knex migration build

--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -1,3 +1,9 @@
 //add code to make project here,
 // refer to module project 4 for sprint-14
 //inside the data/seeds/02-make-recipes area
+
+// const projects = [
+//     {},
+//     {},
+//     {},
+// ]
