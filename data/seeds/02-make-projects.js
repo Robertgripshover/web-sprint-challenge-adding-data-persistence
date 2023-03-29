4544650336693752b8ad0b94b80b6f1ac7b3a64b
Robert Gripshover <robertgrips@gmail.com>
2023-03-29 10:21:19 -0400

    currently building out a seed file to fill up the data

--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -2,8 +2,39 @@
 // refer to module project 4 for sprint-14
 //inside the data/seeds/02-make-recipes area
 
-// const projects = [
-//     {},
-//     {},
-//     {},
-// ]
+const projects = [
+    {project_name: 'build my house', project_description: 'this project will be building a great home'},
+    {project_name: 'fix my car', project_description: 'my car has been broken a while, it will be good to fix it'},
+    {project_name: 'mow the lawn', project_description: 'the grass is getting way to tall'},
+]
+
+const resources = [
+
+]
+
+const tasks = [
+
+]
+
+const project_resources = [
+
+]
+
+exports.seed = async function (knex) {
+    await knex('projects').insert(projects)
+    await knex('resources').insert(resources)
+    await knex('tasks').insert(tasks)
+    await knex('project_resources').insert(project_resources)
+}
+
+
+
+
+
+
+
+
+
+
+
+
